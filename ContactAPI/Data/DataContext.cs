f5c57a98eed22d454b05dfda9b76ae49377fd2c8
pavan <mani365@gmail.com>
2024-01-27 15:13:02 +0530

    data context

---
+++ ContactAPI/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ContactAPI.Models;
+
+namespace ContactAPI.Data
+{
+    public class DataContext:DbContext
+        //dbcontext refers a session with database
+        
+    {
+        //we have to use DbContextOptions as param in this constructor and inherit from base options
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+            
+        }
+        // we created a table with assosciation to contact called Contacts
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
